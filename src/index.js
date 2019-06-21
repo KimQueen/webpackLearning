511c005c5578ab81592cdd98d7613bb0cf87b10f
kim.jin <kim.jin@eisoo.com>
2019-06-21 14:59:51 +0800

    webpack的css资源加载

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import _ from 'lodash'; //用来引入lodash
+import './index.css';
 
 let component = () => {
   let element = document.createElement("div");
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
 
   return element;
 };
